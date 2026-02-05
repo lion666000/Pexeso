a343026ba1e4572ed858296403c0d99642846182
hlajak06 <hlajak06@gasos-ro.cz>
2026-02-05 10:55:17 +0100

    iguanadonos

--- src/main/java/com/example/pexeso/Card.java
+++ src/main/java/com/example/pexeso/Card.java
@@ -38,12 +38,38 @@ public class Card {
         button.setText(String.valueOf(id));
         flipped = true;
 
-
+        String link = "/images/iguana.jpg";
+        switch(id){
+            case 0:
+                link = "/images/ostrich.jpg";
+                break;
+            case 1:
+                link = "/images/tiger.jpg";
+                break;
+            case 2:
+                link = "/images/digger.jpg";
+                break;
+            case 3:
+                link = "/images/iguana.jpg";
+                break;
+            case 4:
+                link = "/images/plank.jpg";
+                break;
+            case 5:
+                link = "/images/gripen.jpg";
+                break;
+            case 6:
+                link = "/images/redwood.jpg";
+                break;
+            case 7:
+                link = "/images/unnamed.jpg";
+                break;
+        }
 
 
         button.setStyle(
                 "-fx-background-image: url('" +
-                        getClass().getResource("/images/iguana.jpg").toExternalForm() +
+                        getClass().getResource(link).toExternalForm() +
                         "');" +
                         "-fx-background-size: cover;"
         );
